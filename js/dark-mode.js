fe2735aea2d21a547880c7c238e6cb56b0993e75
egiiiiil <sixteenth.bird@gmail.com>
2021-05-22 09:47:07 +0200

    Switch spaces to tabs

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,4 +1,4 @@
 let button = document.querySelector( '.dark-mode-button' );
 button.addEventListener( 'click', function () {
-  document.body.classList.toggle( 'dark-mode' );
+	document.body.classList.toggle( 'dark-mode' );
 });
