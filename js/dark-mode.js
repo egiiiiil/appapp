50bd77334ff0ebef42e920d02362f02454c150a4
egiiiiil <sixteenth.bird@gmail.com>
2021-06-11 11:07:43 +0200

    Added meta.js, new menu design, and favicons

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,3 +1,12 @@
+/* let prefersColorSchemeDark = window.matchMedia('(prefers-color-scheme: dark)'); */
+
 $('.dark-mode-button').on( 'click', function() {
 	$(document.body).toggleClass('dark-mode');
 });
+
+/* if (prefersDarkScheme.matches) {
+  $(document.body).toggleClass('dark-mode');
+} else {
+  document.body.classList.remove("dark-mode");
+}
+ */
